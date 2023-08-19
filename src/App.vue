10584f081d422de05e2b33cd54ffac88c78ae01f
rondout <1174051426@qq.com>
2023-08-19 08:57:51 +0800

    chore: 构建工程

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {MyTool, Button} from "@hsf/vue-components"
+import HelloWorld from "./components/HelloWorld.vue";
+import "@hsf/vue-components/dist/style.css"
+
+</script>
+
+<template>
+  <div>
+    <MyTool></MyTool>
+    <Button tip="hanshufei"></Button>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
